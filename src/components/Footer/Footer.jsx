6556800e95faae4f0c2c881f124f46b602063b6f
Saharok0707 <saharok0707@gmail.com>
2022-05-13 13:28:15 +0300

    finish

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import s from './Footer.module.css';
 const Footer = () => {
   return (
     <footer className={s.footerBox}>
-      <p className={s.footerText}>Phonebook © 2022</p>
+      <p className={s.footerText}>Phonebook © 2021 - {format(new Date(), 'yyyy')}</p>
       <p className={s.footerText}>All rights reserved</p>
     </footer>
   );
